0dc6ba1f42f9f24a113f7fce8ba5e2ec9f1881ce
yipeng <986300260@qq.com>
2019-11-16 06:13:48 -0500

    delete compile

--- compile.sh
+++
@@ -1,2 +0,0 @@
-g++ video_transport_server.cpp -o server -I /home/yipeng/thirdlib/glog/include/ -I /home/yipeng/thirdlib/gflags/include/ -I /home/yipeng/thirdlib/opencv/include/ -L /home/yipeng/thirdlib/glog/lib/ -L /home/yipeng/thirdlib/gflags/lib -L /home/yipeng/thirdlib/opencv/lib -lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
-./server -flagfile=server_flagfile_configure
