53c08011f4efdc9f0a528a3b40e050f083b7e908
yipeng <986300260@qq.com>
2019-11-17 09:27:59 -0500

    编译链接客户端脚本 客户端传输本地摄像头数据

--- client_build_with_camera.sh
+++ client_build_with_camera.sh
@@ -1,2 +1,5 @@
-g++ -g -O3 -W -Wall -D local_camera video_transport_client.cpp -o client -I /home/yipeng/thirdlib/glog/include/ -I /home/yipeng/thirdlib/gflags/include/ -I /home/yipeng/thirdlib/opencv/include/ -L /home/yipeng/thirdlib/glog/lib/ -L /home/yipeng/thirdlib/gflags/lib -L /home/yipeng/thirdlib/opencv/lib -lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
+third_party_library=/home/yipeng/thirdlib
+g++ -g -O3 -W -Wall -D local_camera video_transport_client.cpp -o client -I $third_party_library/glog/include/ -I $third_party_library/gflags/include/ \
+-I $third_party_library/opencv/include/ -L $third_party_library/glog/lib/ -L $third_party_library/gflags/lib -L $third_party_library/opencv/lib \
+-lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
 ./client -flagfile=client_flagfile_configure
