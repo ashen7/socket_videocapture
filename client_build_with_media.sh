697e5368c39368da4f49df3a5a66234f230fee53
yipeng <986300260@qq.com>
2019-11-17 09:29:03 -0500

    编译链接客户端脚本 客户端传输本地媒体数据

--- client_build_with_media.sh
+++ client_build_with_media.sh
@@ -1,8 +1,9 @@
-g++ -g -O3 -W -Wall video_transport_client.cpp -o client -I /home/yipeng/thirdlib/glog/include/ -I /home/yipeng/thirdlib/gflags/include/ -I /home/yipeng/thirdlib/opencv/include/ -L /home/yipeng/thirdlib/glog/lib/ -L /home/yipeng/thirdlib/gflags/lib -L /home/yipeng/thirdlib/opencv/lib -lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
+third_party_library=/home/yipeng/thirdlib
+g++ -g -O3 -W -Wall video_transport_client.cpp -o client -I $third_party_library/glog/include/ -I $third_party_library/gflags/include/ \
+-I $third_party_library/opencv/include/ -L $third_party_library/glog/lib/ -L $third_party_library/gflags/lib -L $third_party_library/opencv/lib \
+-lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
 echo -e "\033[1;31mbash脚本开始执行... \033[0m";
 read -p "请输入媒体文件名:" file_name;
-media_path=/home/yipeng/videos/$file_name
-echo $media_path
 
-./client -flagfile=client_flagfile_configure -media_path=$media_path
+./client -flagfile=client_flagfile_configure -media_path=$file_name
 echo -e "\033[1;32mbash脚本执行完毕... \033[0m";
