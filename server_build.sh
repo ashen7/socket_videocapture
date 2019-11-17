69f467e4e8e9dc84333c12689c0b456bcff5cb34
yipeng <986300260@qq.com>
2019-11-17 09:29:34 -0500

    编译链接服务端脚本 服务端6核多线程并行显示6路视频流

--- server_build.sh
+++ server_build.sh
@@ -1,2 +1,5 @@
-g++ -g -O3 -W -Wall video_transport_server.cpp -o server -I /home/yipeng/thirdlib/glog/include/ -I /home/yipeng/thirdlib/gflags/include/ -I /home/yipeng/thirdlib/opencv/include/ -L /home/yipeng/thirdlib/glog/lib/ -L /home/yipeng/thirdlib/gflags/lib -L /home/yipeng/thirdlib/opencv/lib -lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
+third_party_library=/home/yipeng/thirdlib
+g++ -g -O3 -W -Wall  video_transport_server.cpp -o server -I $third_party_library/glog/include/ -I $third_party_library/gflags/include/ \
+-I $third_party_library/opencv/include/ -L $third_party_library/glog/lib/ -L $third_party_library/gflags/lib -L $third_party_library/opencv/lib \
+-lglog -lgflags -lopencv_highgui -lopencv_imgcodecs -lopencv_core -lopencv_imgproc -lopencv_videoio -lopencv_objdetect -lpthread -std=gnu++11
 ./server -flagfile=server_flagfile_configure
